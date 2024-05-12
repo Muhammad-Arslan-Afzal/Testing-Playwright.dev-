f710eaaec64c27317128b2b98a94a059a88654df
Muhammad Arslan Afzal <arslan.afzal@easterngraphics.com>
2024-05-12 13:35:26 +0200

    Uploading obk & comparing name field

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,47 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
-test('pCon-basket', async ({ page }) => {
+  test('pCon-basket', async ({ page }) => {
   await page.goto('https://cd.easterngraphics.com/apps/pcon/pcon.basket-online/wbk/develop/');
+  await page.getByText('Open Project').click();
+    await page.getByText('Click to select').click();
+    const path = require('path');
 
-  const documentCard = await page.locator('.ms-DocumentCard').first().click();
-  
-  await expect(page.getByText('Catalog')).toBeVisible(({ timeout: 10000 }));
-  // await expect(page.getByText('Catalog').click());
-// Click on the "Catalog" element
-  await page.click('text=Catalog');
-  await page.getByLabel("Catalog").isVisible();
+    const [fileChooser] = await Promise.all([
+      page.waitForEvent("filechooser"),
+      await page.locator("div.dropzoneText-165").first().click()  
+    ])
+    await fileChooser.setFiles("club_sofa_100524-131811.obk");
+    
+await page.getByRole('tab', { name: 'Header Data' }).click();
+await page.getByRole('link', { name: 'Quote' }).click();
+    const nameValue = await page.getByLabel('Name').inputValue();
+    console.log("Name : ", nameValue)
+    
+    if (nameValue === "club sofa") {
+  console.log('Name matches expected value');
+} else {
+  console.log('Name does not match expected value');
+    }
+    //check value using builtin matcher/assertion
+    await expect(page.getByLabel('Name')).toHaveValue("club sofa");
 
-  
-
-  // console.log(await page.getByLabel("New Project").click());
-
-
-
-   // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/pCon.login/);
-
-
-  // await page.getByPlaceholder('User name or e-mail').fill('Example Name');
-  // await page.getByPlaceholder('Password').fill('Example Password');
-  // await page.getByRole('button', { name: 'Log in' }).click();
-
-
-  // // Expects page to have a heading with the name of Installation.
-  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-})
+});
